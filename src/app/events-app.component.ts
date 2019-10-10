573a0f96bcd22dee7401a9b0737274e8c1ccc30f
optesis-khk <khk@optesis.com>
2019-10-10 20:14:17 +0000

    [UPDATE] Creating your first data bound

--- src/app/events-app.component.ts
+++ src/app/events-app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-app',
-  template: `
-  <h2>Hello world </h2>
-  <img src="assets/images/basic-shield.png"/>
-  `
+  // template: `
+  // <h2>Hello world </h2>
+  // <img src="assets/images/basic-shield.png"/>
+  // `
+  template:'<events-list></events-list>'
 })
 export class EventsAppComponent {
   title = 'ng-fundamentals';
