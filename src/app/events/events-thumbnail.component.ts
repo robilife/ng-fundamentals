3b4e92bd71d06f8699628415a6045b688ee45caf
optesis-khk <khk@optesis.com>
2019-10-15 21:26:09 +0000

    handling null value

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -5,13 +5,13 @@ import { template } from '@angular/core/src/render3';
     selector: 'event-thumbnail',
     template: `
     <div class="well hoverwell thumbnail">
-        <h2>{{event.name}}</h2>
-        <div>Date: {{event.date}}</div>
-        <div>Time: {{event.time}}</div>
-        <div>Price: \${{event.price}}</div>
+        <h2>{{event?.name}}</h2>
+        <div>Date: {{event?.date}}</div>
+        <div>Time: {{event?.time}}</div>
+        <div>Price: \${{event?.price}}</div>
         <div>
-            <span class="pad-letf">Location: {{event.location.address}}</span>
-            <span>{{event.location.city}}, {{event.location.country}} </span>
+            <span class="pad-letf">Location: {{event?.location?.address}}</span>
+            <span>{{event.location.city}}, {{event?.location?.country}} </span>
         </div>
     </div>
     `,
