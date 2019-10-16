c83fa7f05fd1a90f5808721f90ded0539f20544e
optesis-khk <khk@optesis.com>
2019-10-16 09:37:43 +0000

    styling with ngClass

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -7,20 +7,36 @@ import { template } from '@angular/core/src/render3';
     <div class="well hoverwell thumbnail">
         <h2>{{event?.name}}</h2>
         <div>Date: {{event?.date}}</div>
-        <div>Time: {{event?.time}}</div>
-        <div>Price: \${{event?.price}}</div>
+        <div [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
+            Time: {{event?.time}}
+            <span *ngSwitchCase="'8:00 am'">(Early start)</span>
+            <span *ngSwitchCase="'10:00 am'">(Late start)</span>
+            <span *ngSwitchDefault>(Normal start)</span>
         <div>
-            <span class="pad-letf">Location: {{event?.location?.address}}</span>
-            <span>{{event.location.city}}, {{event?.location?.country}} </span>
+        <div>Price: \${{event?.price}}</div>
+        <div *ngIf="event?.location">
+            <span>Location: {{event?.location?.address}}</span>
+            <span class="pad-letf">{{event?.location?.city}}, {{event?.location?.country}} </span>
+        </div>
+        <div *ngIf="event?.onlineUrl">
+            Online Url: {{event?.onlineUrl}}
         </div>
     </div>
     `,
     styles: [`
-    .thumbnail {min-height: 210px; }
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
+    .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
-    .well div {color: #bbb;}
+    .well div { color: #bbb; }
     `]
 })
 export class EventsThumbnailComponent {
     @Input() event:any;
+
+    getStartTimeClass(){
+        if(this.event && this.event.time === '8:00 am')
+            return ['green', 'bold']
+        return []
+    }
 }
